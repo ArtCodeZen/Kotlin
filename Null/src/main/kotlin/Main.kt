be6cc16db227bb63787eaa7a8783901bef29ef00
artcodezen <henrique.watpequim@gmail.com>
2023-01-18 20:49:00 -0300

    null check length

--- Null/src/main/kotlin/Main.kt
+++ Null/src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
-fun main(args: Array<String>) {
+fun main() {
     // usar o ? depois do tipo para mostrar que podemos por algo nulo na variável!
     var name : String? = "Sandra"
+    println(name)
     name = null
     println(name)
+    name = "Jairo"
+    // println(name.length) mostra uma mensagem de erro pois estamos trabalhando com algo que pode ser nulo
+    // sendo assim para ver o tamanho do length temos que por o ?
+    println(name?.length)
 }
