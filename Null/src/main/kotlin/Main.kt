df4e1f08f087f391706809364034faf2736d516a
artcodezen <henrique.watpequim@gmail.com>
2023-01-18 20:41:04 -0300

    null example

---
+++ Null/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    // usar o ? depois do tipo para mostrar que podemos por algo nulo na variável!
+    var name : String? = "Sandra"
+    name = null
+    println(name)
+}
