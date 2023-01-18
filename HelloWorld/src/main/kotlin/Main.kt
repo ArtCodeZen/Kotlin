a2d67483ba0dc388ff95f040ae6d0de27b9add97
artcodezen <henrique.watpequim@gmail.com>
2023-01-18 16:11:40 -0300

    aprendendo valores e binários

--- HelloWorld/src/main/kotlin/Main.kt
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -1,8 +1,31 @@
+fun plusOne(x : Int) = x + 1
+
 fun main() {
     println("Hello World!")
-    val x = 10
+    var x = 10
+    x = plusOne(10)
     System.out.println(x.toString())
 
+    // tipos de dados
+    var int = 10
+    var string = "ola"
+    var float = 10.2
 
+    println(int)
+    println(string)
+    println(float)
 
+    // float to double
+    println("Float to double:" + float.toDouble())
+
+    // shift left método binário
+    var shift = 1 shl 1
+    println("Shift valor 1: " + shift)
+
+    var flg = 1 and 0x11
+    println("flag: " + flg)
+
+    // invert
+    val inv = 1.inv()
+    println(inv)
 }
