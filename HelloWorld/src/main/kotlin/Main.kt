a738fd0b2b2c9010ad2a137719fe83b3608fde86
artcodezen <henrique.watpequim@gmail.com>
2023-01-18 16:33:06 -0300

    arrays teste e comentários

--- HelloWorld/src/main/kotlin/Main.kt
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -28,4 +28,29 @@ fun main() {
     // invert
     val inv = 1.inv()
     println(inv)
+
+    // if compare - comparar se é verdadeiro ou falso!
+    var i = 10;
+    var j = 20;
+    println("valor de i: " + i + " valor de j: " + j)
+    println("i < j: " + (i < j).toString())
+    println("i > j: " + (i > j).toString())
+    println("i < j && j > i: " + (i < j && j > i).toString())
+
+    // chars
+    println("Meu texto exemplo\nsalto de linha")
+
+    // arrays
+    val array  = arrayOf("banana", "maçã", "ervilha")
+    println(array[0])
+
+    val perfectSquares = Array(3, {k->k * 10})
+    println(perfectSquares[2])
+
+    // comentários
+    /*comentário extenso
+    *
+    * teste
+    * teste
+    * */
 }
