aabf279df8f89943274450a6a0f10dcd0ce92a1a
artcodezen <henrique.watpequim@gmail.com>
2023-01-31 17:51:28 -0300

    usando when substituto do switch case

---
+++ When/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun whenWhithouArgs(x: Int, y :Int) : Boolean{
+    when {
+        x < y -> return true
+        x > y -> return false
+        else -> return false
+    }
+}
+
+fun main() {
+    println("When")
+    val x = whenWhithouArgs(10, 10)
+    println(x)
+}
