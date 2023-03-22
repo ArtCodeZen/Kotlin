e9e799c7ab35389ac86e462067f468ecd20d3353
artcodezen <henrique.watpequim@gmail.com>
2023-03-22 09:20:39 -0300

    Expressoes lambda

---
+++ ExpressoesLambda/untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+fun main() {
+
+    val doceDeBanana: (Int) -> String = { quantidade ->
+        "x$quantidade de doce de banana expressao indireta"
+    }
+    val myFun = ::printMessage
+    myFun()
+
+    val myAlert = printMessageAlert
+    myAlert()
+    happyMessage()
+    val functionAltoBaixo = altoOuBaixo(true, doceDeBanana)
+    val functionAltoBaixoFinal = altoOuBaixo(true, {"x$it de doce de babana expressao direta"})
+    val functionAltoBaixoResumida = altoOuBaixo(true) { "x$it de doce de babana expressao final" }
+    // usando o repeat
+    repeat(times = 3){
+        functionAltoBaixo()
+    }
+
+
+}
+fun printMessage(){
+    println("OK!")
+}
+val printMessageAlert ={
+    println("Alert OK!")
+}
+// Unit significa não retorna nada!
+val happyMessage: ()->Unit = {
+    println("Unidade vazia, legal")
+}
+
+fun altoOuBaixo(isAlto: Boolean, outroDoce: (Int) -> String) : ()->Unit{
+    if(isAlto){
+
+        println(outroDoce(5))
+        return alto
+    }
+    else{
+        return baixo
+    }
+}
+
+val alto = {
+    println("Alto".uppercase())
+}
+val baixo = {
+    println("Baixo".uppercase())
+}
